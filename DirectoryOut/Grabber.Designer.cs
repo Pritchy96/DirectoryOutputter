31fbbba44f1b614ab09fffe7725b8713c5547fcf
Tom Pritchard <tompritchard1996@gmail.com>
2014-07-05 00:37:27 +0100

    Outputs a directory as a list: GUI Version

---
+++ DirectoryOut/Grabber.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace DirectoryOut
+{
+    partial class Grabber
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browser = new System.Windows.Forms.FolderBrowserDialog();
+            this.chkSearchSub = new System.Windows.Forms.CheckBox();
+            this.chkFullPath = new System.Windows.Forms.CheckBox();
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chkSearchSub
+            // 
+            this.chkSearchSub.AutoSize = true;
+            this.chkSearchSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkSearchSub.Location = new System.Drawing.Point(16, 17);
+            this.chkSearchSub.Name = "chkSearchSub";
+            this.chkSearchSub.Size = new System.Drawing.Size(145, 20);
+            this.chkSearchSub.TabIndex = 0;
+            this.chkSearchSub.Text = "Search Subfolders?";
+            this.chkSearchSub.UseVisualStyleBackColor = true;
+            // 
+            // chkFullPath
+            // 
+            this.chkFullPath.AutoSize = true;
+            this.chkFullPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkFullPath.Location = new System.Drawing.Point(16, 44);
+            this.chkFullPath.Name = "chkFullPath";
+            this.chkFullPath.Size = new System.Drawing.Size(102, 20);
+            this.chkFullPath.TabIndex = 1;
+            this.chkFullPath.Text = "List full path?";
+            this.chkFullPath.UseVisualStyleBackColor = true;
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChooseFile.Location = new System.Drawing.Point(42, 85);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(119, 56);
+            this.btnChooseFile.TabIndex = 2;
+            this.btnChooseFile.Text = "Choose a Directory";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(205, 190);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.chkSearchSub);
+            this.tabPage1.Controls.Add(this.btnChooseFile);
+            this.tabPage1.Controls.Add(this.chkFullPath);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(197, 164);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(197, 164);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Grabber
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(205, 190);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Grabber";
+            this.Text = "DirectoryMapper";
+            this.Load += new System.EventHandler(this.Grabber_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog browser;
+        private System.Windows.Forms.CheckBox chkSearchSub;
+        private System.Windows.Forms.CheckBox chkFullPath;
+        private System.Windows.Forms.Button btnChooseFile;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ DirectoryOut/Grabber.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DirectoryOut
+{
+    public partial class Grabber : Form
+    {
+        public Grabber()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Grabber_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+            SearchOption searchOption = new SearchOption();
+
+            if (chkSearchSub.Checked)
+            {
+                searchOption = SearchOption.AllDirectories;
+            }
+            else
+            {
+                searchOption = SearchOption.TopDirectoryOnly;
+            }
+
+            browser.ShowNewFolderButton = false;
+            browser.Description = "Please select a folder for file listing:";
+            browser.ShowDialog();
+            string path = browser.SelectedPath.ToString();
+            string[] log = Directory.GetFiles(path, ".", searchOption);
+
+            if (!chkFullPath.Checked)
+            {
+                for (int i = 0; i < log.Count(); i++)
+                {
+                    log[i] = log[i].Remove(0, path.Count());
+                }
+            }
+            File.WriteAllLines(path + "\\Output.txt", log);
+            Application.Exit();
+        }
+
+    }
+}
